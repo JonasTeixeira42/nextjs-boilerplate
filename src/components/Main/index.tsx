d02e3e71e18aa21bb09470a5959d78c0886e67b0
Jonas <jonaslagoteixeira@gmail.com>
2020-10-05 17:05:00 -0300

    foi atualizado snapshot do component main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 import * as S from './styles';
 
-const Main: React.FC = () => {
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJs, NextJS, Styled Components',
+}) => {
   return (
     <S.Wrapper>
       <S.Logo
         src="img/logo.svg"
         alt="Imagem de um átomo e React avançado escrito ao lado"
       />
-      <S.Title>React Avançado</S.Title>
-      <S.Description>
-        TypeScript, ReactJs, NextJS, Styled Components
-      </S.Description>
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
       <S.Illustration
         src="img/hero-illustration.svg"
         alt="Um desenvolvedor de frente para a tela do código"
