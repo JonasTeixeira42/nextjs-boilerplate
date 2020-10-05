78e246b17a4d1ab9a3358e621198c034d4505077
Jonas <jonaslagoteixeira@gmail.com>
2020-10-05 18:19:17 -0300

    foi adicionado addons essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,5 @@ module.exports = {
     '../src/**/*.stories.mdx',
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-  ],
+  addons: ['@storybook/addon-essentials'],
 };
--- src/components/Main/stories.stories.tsx
+++ src/components/Main/stories.stories.tsx
@@ -1,19 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
+
 import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJs, NextJS, Styled Components',
-    )}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
